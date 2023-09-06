41f14b8817dd71e76f574168aff3d683aa17c5c1
xjwsx <134588087+xjwsx@users.noreply.github.com>
2023-09-06 22:21:06 +0900

    timetxt
    timetxt

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public TextMeshProUGUI timeTxt;
+    float time = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeTxt.text = time.ToString("N2");
+    }
+}
